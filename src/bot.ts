67b90f5285efd4af0dc0b4709322cec561dc7b56
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-21 00:03:42 +0300

    Initial commit

---
+++ src/bot.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+
+import { Bot } from 'grammy'
+
+const bot = new Bot(process.env.BOT_TOKEN as string)
+
+bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
+bot.on('message', (ctx) => ctx.reply('Got another message!'))
+
+bot.start().catch(error => console.error(error))
