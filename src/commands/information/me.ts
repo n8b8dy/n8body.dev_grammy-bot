9f254a863d1b1886d6bc8870ecaab79f45c085cc
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-21 19:24:34 +0300

    Feat: Send profile photos on /me

--- src/commands/information/me.ts
+++ src/commands/information/me.ts
@@ -1,18 +1,36 @@
 import type { CommandMiddleware, Context } from 'grammy'
+import { GrammyError, InputMediaBuilder } from 'grammy'
 import { eRT } from '@/utils/text'
 
 export const meHandler: CommandMiddleware<Context> = async (ctx) => {
+  if (!ctx.from) return
+
   const text = [
-    `<b>Username:</b> <code>${eRT(ctx.from?.username)}</code>`,
-    `<b>ID</b>: <code>${eRT(ctx.from?.id)}</code>`,
-    `First name: ${eRT(ctx.from?.first_name)}`,
-    `Last name: ${eRT(ctx.from?.last_name)}`,
-    `Bot: ${eRT(ctx.from?.is_bot)}`,
-    `Premium: ${eRT(ctx.from?.is_premium)}`,
+    `<b>Username:</b> <code>${eRT(ctx.from.username)}</code>`,
+    `<b>ID</b>: <code>${eRT(ctx.from.id)}</code>`,
+    `First name: ${eRT(ctx.from.first_name)}`,
+    `Last name: ${eRT(ctx.from.last_name)}`,
+    `Bot: ${eRT(ctx.from.is_bot)}`,
+    `Premium: ${eRT(ctx.from.is_premium)}`,
   ].join('\n')
 
-  await ctx.reply(text, {
-    parse_mode: 'HTML',
-    reply_parameters: { message_id: ctx.msg.message_id },
-  })
+  try {
+    const profilePhotos = await ctx.api.getUserProfilePhotos(ctx.from.id)
+    const mediaGroup  = profilePhotos.photos.slice(0, 10).map(photoSize => InputMediaBuilder.photo(photoSize[0].file_id))
+
+    mediaGroup[0].caption = text
+    mediaGroup[0].parse_mode = 'HTML'
+
+    await ctx.replyWithMediaGroup(mediaGroup, {
+      reply_parameters: { message_id: ctx.msg.message_id },
+    })
+
+  } catch (error) {
+    if (error instanceof GrammyError) return await ctx.reply(text, {
+      parse_mode: 'HTML',
+      reply_parameters: { message_id: ctx.msg.message_id },
+    })
+
+    throw error
+  }
 }
--- src/utils/text.ts
+++ src/utils/text.ts
@@ -2,5 +2,5 @@ import { escape } from 'html-escaper'
 
 // escapeReplyText
 export function eRT(text?: string | number | boolean, placeholder = '<i>[DATA EXPUNGED]</i>'){
-  return text === undefined ? placeholder: escape(String(text))
+  return text === undefined ? placeholder : escape(String(text))
 }
