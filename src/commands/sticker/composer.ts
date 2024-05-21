364019ff52b130adce0cb1ea3404cf891d3bc2b5
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-21 20:17:45 +0300

    Feat: Send sticker id on message:sticker

--- src/commands/sticker/composer.ts
+++ src/commands/sticker/composer.ts
@@ -4,3 +4,13 @@ import { stickerHandler } from '@/commands/sticker/sticker'
 export const sticker = new Composer()
 
 sticker.command('sticker', stickerHandler)
+sticker.on('message:sticker', async (ctx) => {
+  if (ctx.chat.type !== 'private') return
+
+  const text = `Sticker's ID is: <code>${ctx.msg.sticker.file_id}|</code>`
+
+  await ctx.reply(text, {
+    parse_mode: 'HTML',
+    reply_parameters: { message_id: ctx.msg.message_id },
+  })
+})
