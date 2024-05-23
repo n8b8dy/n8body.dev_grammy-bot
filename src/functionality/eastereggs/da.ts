ce2eecfe5a82805003a5c6e700dcf8d072ec4de6
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-23 22:41:47 +0300

    Fix: Add 80% chance daHandler to ignore message:text

--- src/functionality/eastereggs/da.ts
+++ src/functionality/eastereggs/da.ts
@@ -5,6 +5,8 @@ export async function daHandler(ctx: Filter<BotContext, 'message:text'>) {
   const match = ctx.msg.text.match(/\s*([dд]+[aа]+\W*)$/i)
   if (!match) return
 
+  if (Math.random() > 0.2) return
+
   await ctx.reply(`Пиз${match[1].toLowerCase()}`, {
     reply_parameters: { message_id: ctx.msgId }
   })
