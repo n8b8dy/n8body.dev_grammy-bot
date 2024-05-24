40854ac3a0c7c8620fbbd0be97c744127732fa0a
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-24 04:47:25 +0300

    Start using cjs output

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,12 @@
+import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import { typescriptPaths } from 'rollup-plugin-typescript-paths'
 
 export default {
-  plugins: [typescript(), typescriptPaths()],
+  plugins: [commonjs(), typescript(), typescriptPaths()],
   input: 'src/bot.ts',
   output: {
-    file: `build/bot.js`,
-    format: 'es',
+    file: `dist/bot.cjs`,
+    format: 'cjs',
   },
 }
