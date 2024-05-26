22adfff51baa2093dec650eddabcf1dd62e885d2
n8b8dy <ruslanchernomorski@gmail.com>
2024-05-26 12:39:31 +0300

    Feat: Create removeStartStickerHandler

--- src/functionality/creator/composer.ts
+++ src/functionality/creator/composer.ts
@@ -3,7 +3,9 @@ import type { BotContext } from '@/types/bot'
 import { Composer } from 'grammy'
 
 import { addStartStickerHandler } from '@/functionality/creator/addStartSticker'
+import { removeStartStickerHandler } from '@/functionality/creator/removeStartSticker'
 
 export const creator = new Composer<BotContext>()
 
 creator.command(['addStartSticker', 'addStartStickers'], addStartStickerHandler)
+creator.command(['removeStartSticker', 'removeStartStickers'], removeStartStickerHandler)
