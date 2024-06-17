4c5266bd17c8d166b64dfbecd1c9d7a8eae72862
n8b8dy <ruslanchernomorski@gmail.com>
2024-06-18 00:15:41 +0300

    Feat: Create tyMessageHandler

--- src/functionality/eastereggs/composer.ts
+++ src/functionality/eastereggs/composer.ts
@@ -4,10 +4,12 @@ import { Composer } from 'grammy'
 
 import { daMessageHandler } from '@/functionality/eastereggs/da_message'
 import { yaMessageHandler } from '@/functionality/eastereggs/ya_message'
+import { tyMessageHandler } from '@/functionality/eastereggs/ty_mesage'
 import { endCommandHandler } from '@/functionality/eastereggs/end_command'
 
 export const eastereggs = new Composer<BotContext>()
 
 eastereggs.on('message:text', daMessageHandler)
 eastereggs.on('message:text', yaMessageHandler)
+eastereggs.on('message:text', tyMessageHandler)
 eastereggs.command('end', endCommandHandler)
---
+++ src/functionality/eastereggs/ty_mesage.ts
@@ -0,0 +1,13 @@
+import type { Filter, NextFunction } from 'grammy'
+import type { BotContext } from '@/types/bot'
+
+export async function tyMessageHandler(ctx: Filter<BotContext, 'message:text'>, next: NextFunction) {
+  const match = ctx.msg.text.match(/\S*([тt]+ы+[\s.,!?\\()01:;"']*)$/i)
+  if (!match) return await next()
+
+  if (Math.random() > 0.2) return await next()
+
+  await ctx.reply(`Затычка от пиз${match[1].toLowerCase()}`, {
+    reply_parameters: { message_id: ctx.msgId },
+  })
+}
