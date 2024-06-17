0e2462d2241f19aa23c8f3939f1c727001e229ee
n8b8dy <ruslanchernomorski@gmail.com>
2024-06-18 00:10:01 +0300

    Feat: Make da and ya eastereggs less common

--- src/functionality/eastereggs/ya_message.ts
+++ src/functionality/eastereggs/ya_message.ts
@@ -5,7 +5,7 @@ export async function yaMessageHandler(ctx: Filter<BotContext, 'message:text'>, 
   const match = ctx.msg.text.match(/\s*((?:[yй]+[aа]+|я+)[\s.,!?\\()01:;"']*)$/i)
   if (!match) return await next()
 
-  if (Math.random() > 0.2) return await next()
+  if (Math.random() > 0.1) return await next()
 
   await ctx.reply(`Головка от ху${match[1].toLowerCase()}`, {
     reply_parameters: { message_id: ctx.msgId },
