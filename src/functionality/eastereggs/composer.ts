e8811c8bf3fa35d088301e7a83f19f5ee0fc6dfb
n8b8dy <ruslanchernomorski@gmail.com>
2024-06-28 23:23:35 +0300

    Fix: Add nuMessageHandler to composer

--- src/functionality/eastereggs/composer.ts
+++ src/functionality/eastereggs/composer.ts
@@ -6,10 +6,12 @@ import { daMessageHandler } from '@/functionality/eastereggs/da_message'
 import { yaMessageHandler } from '@/functionality/eastereggs/ya_message'
 import { tyMessageHandler } from '@/functionality/eastereggs/ty_mesage'
 import { endCommandHandler } from '@/functionality/eastereggs/end_command'
+import { nuMessageHandler } from '@/functionality/eastereggs/nu_message'
 
 export const eastereggs = new Composer<BotContext>()
 
 eastereggs.on('message:text', daMessageHandler)
 eastereggs.on('message:text', yaMessageHandler)
 eastereggs.on('message:text', tyMessageHandler)
+eastereggs.on('message:text', nuMessageHandler)
 eastereggs.command('end', endCommandHandler)
